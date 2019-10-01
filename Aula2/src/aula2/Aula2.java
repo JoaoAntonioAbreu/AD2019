5de45ad11182a1bb73869027101be4a91aa76ed0
JoaoAbreu <43825149+JoaoAntonioAbreu@users.noreply.github.com>
2019-10-01 19:59:57 +0100

    ficha2 update

--- Aula2/src/aula2/Aula2.java
+++ Aula2/src/aula2/Aula2.java
@@ -70,6 +70,108 @@ public class Aula2 {
         else {
             System.out.println("Number isnt prime");
         }
-    } 
+    }
+    
+    public static String toString (int[]numeros){
+        String resultado = "";
+        for (int i = 0; i < numeros.length; i++){
+            resultado += numeros[i]+","; 
+        }   
+        return resultado;
+    }
+    
+    public static int maximumOf (int[]numeros){
+        int max = numeros[0];
+        for(int i = 0; i<numeros.length; i++){
+            if(numeros[i]> max){
+                max = numeros[i];
+            }
+        }
+        return max;
+    }
+    
+    
+    public static int minimumOf (int[]numeros){
+        int min = numeros[0];
+        for(int i = 0; i<numeros.length; i++){
+            if(numeros[i]< min){
+                min = numeros[i];
+            }
+        }
+        return min;
+    }
+    
+    public static int[] copyOf(int[]numeros){
+        int[]copy = new int[numeros.length];
+        for (int i = 0; i < numeros.length; i++){
+            copy[i] = numeros[i];
+        }
+        return copy;
+    }
+    
+    public static boolean contains(int[]numeros, int n){
+        boolean exist = false;
+        for (int i = 0 ; i<numeros.length; i++){
+            if (numeros[i]==n){
+                exist = true;
+            }
+        }
+        return exist;
+    }
+    
+    public static boolean containsDuplicates(int[]numeros){
+        boolean exist = false;
+        for(int i = 0; i<numeros.length;i++){
+           for(int j=i+1;j<numeros.length;j++){
+               if(numeros[i]==numeros[j]){
+                   exist = true;
+               }
+           }
+        }
+        return exist;
+    }
+    
+    public static int indexOf(int[] numeros, int n){
+        int index=0;
+        for(int i = 0;i<numeros.length;i++){
+            if (n==numeros[i]){
+                index = i;
+                break;
+            }else{
+                index = -1;
+            }
+        }
+        return index;
+    }
+    
+    public static int[] add(int[]numeros, int n){
+       int[] novo = new int[numeros.length+1];
+       for(int i = 0; i<numeros.length;i++){
+           novo[i]=numeros[i];
+           if(i==numeros.length-1){
+               novo[i+1]=n;
+           }
+       }
+       return novo;
+    }
+    
+    public static int[] remove(int[]numeros,int n){
+        int quantos=0;
+        int pos=0;
+        
+        for(int i=0;i<numeros.length;i++){
+            if(numeros[i]==n){
+               quantos++;
+            }
+        }
+        int[]novo = new int[numeros.length-quantos];
+        for(int i = 0; i< numeros.length;i++){
+           if(numeros[i]!=n){
+               novo[pos]=numeros[i];
+               pos++;
+           }
+       }
+        return novo;
+    }
 }
 
--- Aula2/src/aula2/Main.java
+++ Aula2/src/aula2/Main.java
@@ -5,12 +5,15 @@
  */
 package aula2;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Turma A
  */
 public class Main {
     public static void main(String[] args){
+        int numeros[] = {10,20,1,30,2,10};
         int ex1 = Aula2.powerOf(2, 10);
         System.out.println(ex1);
         int ex2 = Aula2.sumOfNaturalNumbersUpTo(3);
@@ -22,5 +25,23 @@ public class Main {
         int ex5 = Aula2.numberOfDivisorsOf(10);
         System.out.println(ex5);
         Aula2.isPrime(4);
+        String ex6 = Aula2.toString(numeros);
+        System.out.println(ex6);
+        int ex7 = Aula2.maximumOf(numeros);
+        System.out.println(ex7);
+        int ex8 = Aula2.minimumOf(numeros);
+        System.out.println(ex8);
+        int[] ex9 = Aula2.copyOf(numeros);
+        System.out.println(Arrays.toString(ex9));
+        boolean ex10 = Aula2.contains(numeros, 20);
+        System.out.println(ex10);
+        boolean ex11 = Aula2.containsDuplicates(numeros);
+        System.out.println(ex11);
+        int ex12 = Aula2.indexOf(numeros, 2);
+        System.out.println(ex12);
+        int[] ex13 = Aula2.add(numeros, 414);
+        System.out.println(Arrays.toString(ex13));
+        int[] ex14= Aula2.remove(numeros,10);
+        System.out.println(Arrays.toString(ex14));
     }
 }
