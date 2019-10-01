5c4253ae30caa7e26dcd54a637874e73275512cf
JoaoAbreu <43825149+JoaoAntonioAbreu@users.noreply.github.com>
2019-10-01 20:41:18 +0100

    ficha 2 final update

--- Aula2/src/aula2/Aula2.java
+++ Aula2/src/aula2/Aula2.java
@@ -14,164 +14,162 @@ public class Aula2 {
     /**
      * @param args the command line arguments
      */
-    public static int powerOf (int a, int b){
-        int resultado=1;
-        for(int i=0;i<b;i++){
-            resultado = resultado*a;
+    public static int powerOf(int a, int b) {
+        int resultado = 1;
+        for (int i = 0; i < b; i++) {
+            resultado = resultado * a;
         }
-      return resultado; 
+        return resultado;
     }
-    
-    public static int sumOfNaturalNumbersUpTo (int n){
+
+    public static int sumOfNaturalNumbersUpTo(int n) {
         int resultado = 0;
-        for(int i = 0; i<=n;i++){
+        for (int i = 0; i <= n; i++) {
             resultado = resultado + i;
         }
         return resultado;
     }
-    
-    public static int sumOfNaturalNumbersBetween(int n1, int n2){
+
+    public static int sumOfNaturalNumbersBetween(int n1, int n2) {
         int resultado = 0;
-        for(int i = n1; i<=n2;i++){
+        for (int i = n1; i <= n2; i++) {
             resultado = resultado + i;
-            
+
         }
         return resultado;
     }
-    
-    public static int sumOfEvenNumbersBetween(int n1, int n2){
+
+    public static int sumOfEvenNumbersBetween(int n1, int n2) {
         int resultado = 0;
-        for (int i = n1;i<=n2;i++){
-            if (i%2==0){
+        for (int i = n1; i <= n2; i++) {
+            if (i % 2 == 0) {
                 resultado = resultado + i;
             }
         }
         return resultado;
     }
-    
-    public static int  numberOfDivisorsOf(int n1){
+
+    public static int numberOfDivisorsOf(int n1) {
         int resultado = 0;
-        for(int i = 1; i<=n1;i++){
-            if(n1%i==0){
+        for (int i = 1; i <= n1; i++) {
+            if (n1 % i == 0) {
                 resultado++;
             }
         }
         return resultado;
     }
-    
-    public static void isPrime(int n1){
+
+    public static void isPrime(int n1) {
         int resultado = 0;
-        for (int i=1;i<=n1;i++){
-            if(n1%i==0){
+        for (int i = 1; i <= n1; i++) {
+            if (n1 % i == 0) {
                 resultado++;
             }
-        }if(resultado ==2){
-            System.out.println("Number is prime");}
-        else {
+        }
+        if (resultado == 2) {
+            System.out.println("Number is prime");
+        } else {
             System.out.println("Number isnt prime");
         }
     }
-    
-    public static String toString (int[]numeros){
+
+    public static String toString(int[] numeros) {
         String resultado = "";
-        for (int i = 0; i < numeros.length; i++){
-            resultado += numeros[i]+","; 
-        }   
+        for (int i = 0; i < numeros.length; i++) {
+            resultado += numeros[i] + ",";
+        }
         return resultado;
     }
-    
-    public static int maximumOf (int[]numeros){
+
+    public static int maximumOf(int[] numeros) {
         int max = numeros[0];
-        for(int i = 0; i<numeros.length; i++){
-            if(numeros[i]> max){
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > max) {
                 max = numeros[i];
             }
         }
         return max;
     }
-    
-    
-    public static int minimumOf (int[]numeros){
+
+    public static int minimumOf(int[] numeros) {
         int min = numeros[0];
-        for(int i = 0; i<numeros.length; i++){
-            if(numeros[i]< min){
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < min) {
                 min = numeros[i];
             }
         }
         return min;
     }
-    
-    public static int[] copyOf(int[]numeros){
-        int[]copy = new int[numeros.length];
-        for (int i = 0; i < numeros.length; i++){
+
+    public static int[] copyOf(int[] numeros) {
+        int[] copy = new int[numeros.length];
+        for (int i = 0; i < numeros.length; i++) {
             copy[i] = numeros[i];
         }
         return copy;
     }
-    
-    public static boolean contains(int[]numeros, int n){
+
+    public static boolean contains(int[] numeros, int n) {
         boolean exist = false;
-        for (int i = 0 ; i<numeros.length; i++){
-            if (numeros[i]==n){
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == n) {
                 exist = true;
             }
         }
         return exist;
     }
-    
-    public static boolean containsDuplicates(int[]numeros){
+
+    public static boolean containsDuplicates(int[] numeros) {
         boolean exist = false;
-        for(int i = 0; i<numeros.length;i++){
-           for(int j=i+1;j<numeros.length;j++){
-               if(numeros[i]==numeros[j]){
-                   exist = true;
-               }
-           }
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = i + 1; j < numeros.length; j++) {
+                if (numeros[i] == numeros[j]) {
+                    exist = true;
+                }
+            }
         }
         return exist;
     }
-    
-    public static int indexOf(int[] numeros, int n){
-        int index=0;
-        for(int i = 0;i<numeros.length;i++){
-            if (n==numeros[i]){
+
+    public static int indexOf(int[] numeros, int n) {
+        int index = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (n == numeros[i]) {
                 index = i;
                 break;
-            }else{
+            } else {
                 index = -1;
             }
         }
         return index;
     }
-    
-    public static int[] add(int[]numeros, int n){
-       int[] novo = new int[numeros.length+1];
-       for(int i = 0; i<numeros.length;i++){
-           novo[i]=numeros[i];
-           if(i==numeros.length-1){
-               novo[i+1]=n;
-           }
-       }
-       return novo;
+
+    public static int[] add(int[] numeros, int n) {
+        int[] novo = new int[numeros.length + 1];
+        for (int i = 0; i < numeros.length; i++) {
+            novo[i] = numeros[i];
+        }
+        novo[numeros.length] = n;
+
+        return novo;
     }
-    
-    public static int[] remove(int[]numeros,int n){
-        int quantos=0;
-        int pos=0;
-        
-        for(int i=0;i<numeros.length;i++){
-            if(numeros[i]==n){
-               quantos++;
+
+    public static int[] remove(int[] numeros, int n) {
+        int quantos = 0;
+        int pos = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == n) {
+                quantos++;
             }
         }
-        int[]novo = new int[numeros.length-quantos];
-        for(int i = 0; i< numeros.length;i++){
-           if(numeros[i]!=n){
-               novo[pos]=numeros[i];
-               pos++;
-           }
-       }
+        int[] novo = new int[numeros.length - quantos];
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] != n) {
+                novo[pos++] = numeros[i];
+                //pos++;
+            }
+        }
         return novo;
     }
 }
-
